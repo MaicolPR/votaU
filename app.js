73af483f305d9c00ee0e389180a83e64efef45a5
MaicolPR <64278367+MaicolPR@users.noreply.github.com>
2020-04-24 12:56:27 -0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,48 @@
+var app = angular.module("votaU",['ngRoute']);
+
+app.controller("voto", ["$scope", function($scope){
+	$scope.UrnaDelDia = 'Ruta marcha';
+
+	$scope.OpcionesUrna = [
+		{nombre: 'Calle 26 --> 7ma --> plaza', idOpcion: 1},
+		{nombre: 'Calle 45 --> Caracas --> Heroes', idOpcion: 2},
+		{nombre: 'NQS --> AutoNorte --> Calle 106', idOpcion: 3},
+		{nombre: 'Voto en blanco', idOpcion: 4}
+	];
+
+	$scope.votos = [
+	];
+
+	$scope.admin = 'votaUadmin';
+	$scope.listaVotos = false;
+	$scope.mostrarLista = false;
+	$scope.idvoto = 1;
+	$scope.estado = 'Valido';
+
+	$scope.ingresar = function(){
+		if($scope.usuarioVoto == $scope.admin){
+			$scope.listaVotos = true;
+		}else{
+			$scope.mostrarLista= true;
+			$scope.listaVotos = false;
+
+		}
+	};
+
+	$scope.tomarDato = function(op) {
+		$scope.votos.push({usuario: $scope.usuarioVoto, eleccion: op.nombre, h: $scope.estado, numero: $scope.idvoto});
+		$scope.mostrarLista = false;
+		alert('Su voto ha sido guardado');
+		$scope.idvoto ++;
+	};
+
+	$scope.anularVoto = function(){
+		for(i = 0; i < votos.lenght; i++){
+			if($scope.votoAnular == $scope.votos.numero[i]){
+				$scope.votos.h = 'Nulo';
+				i = votos.lenght;
+			}
+		}
+	};
+
+}])
